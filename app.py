f12bf97fbcea6326c58f38f4bcf958cfa056de7e
Joao Pedro Rocha Vidigal <0000109532066xsp@aluno.educacao.sp.gov.br>
2026-05-13 15:56:53 -0300

    Bug inesperado

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
-import os
-# O código busca a senha no ambiente (Cofre)
-senha_do_banco = os.getenv('SENHA_BANCO', 'NAO_CONFIGURADA')
-print("--- 🏦 SISTEMA DO BANCO DIGITAL ---")
-if senha_do_banco == '12345':
-    print("✅ Senha Correta! Acesso liberado ao cofre.")
-else:
-    print("❌ ACESSO NEGADO! Chave de segurança não encontrada.")
+print("🚨 ERRO CRÍTICO! SISTEMA FORA DO AR!")
+exit(1)
